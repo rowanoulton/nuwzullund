3bc6c159496772c5f603d2d12a59c888b3a9139b
Sarah Hui <sarah@vendhq.com>
2014-07-18 17:22:59 +1200

    Update to standards

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express         = require('express'),
+    path            = require('path'),
+    favicon         = require('static-favicon'),
+    logger          = require('morgan'),
+    cookieParser    = require('cookie-parser'),
+    bodyParser      = require('body-parser'),
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+    routes          = require('./routes/index'),
+    users           = require('./routes/users'),
 
-var app = express();
+    app             = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,9 +25,9 @@ app.use('/', routes);
 app.use('/users', users);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
+app.use(function (req, res, next) {
+    var err     = new Error('Not Found');
+    err.status  = 404;
     next(err);
 });
 
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -47,13 +47,12 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
-
 
 module.exports = app;
