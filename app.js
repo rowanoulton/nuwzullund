57242a4d523e36284e8c206414fee02ebe589a8f
Sarah Hui <sarah@vendhq.com>
2014-07-18 17:26:37 +1200

    Update doc sections

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/*
+ * Dependencies & app setup
+ */
 var express         = require('express'),
     path            = require('path'),
     favicon         = require('static-favicon'),
@@ -10,7 +13,9 @@ var express         = require('express'),
 
     app             = express();
 
-// view engine setup
+/*
+ * Express setup
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -21,20 +26,22 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
+ * Routes
+ */
 app.use('/', routes);
 app.use('/users', users);
 
-/// catch 404 and forward to error handler
+/*
+ * Express error handlers
+ */
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err     = new Error('Not Found');
     err.status  = 404;
     next(err);
 });
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
+// Development error handler, will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
@@ -44,9 +51,7 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
-// production error handler
-// no stacktraces leaked to user
+// Production error handler, no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
